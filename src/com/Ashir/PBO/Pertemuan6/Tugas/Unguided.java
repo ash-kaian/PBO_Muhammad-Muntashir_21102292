b3fe3cc1f62a30543c184d0437a4410f276e086e
kakeru05 <rukake2@gmail.com>
2023-05-26 22:07:24 +0700

    commit

--- src/com/Ashir/PBO/Pertemuan6/Tugas/Unguided.java
+++ src/com/Ashir/PBO/Pertemuan6/Tugas/Unguided.java
@@ -4,8 +4,8 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Unguided {
-    public static int BagiBilangan(int bilangan1, int bilangan2) {
-        return (int) bilangan1/bilangan2;
+    public static double BagiBilangan(int bilangan1, int bilangan2) {
+        return (double) bilangan1/bilangan2;
     }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -17,8 +17,8 @@ public class Unguided {
             System.out.print("Masukkan bilangan kedua   : ");
             int bilangan2 = input.nextInt();
 
-            int hasil = BagiBilangan(bilangan1, bilangan2);
-            System.out.println(bilangan1 + " / " + bilangan2 + " = " + hasil + " (dibulatkan)");
+            double hasil = BagiBilangan(bilangan1, bilangan2);
+            System.out.println(bilangan1 + " / " + bilangan2 + " = " + hasil);
         }
 
         catch (ArithmeticException e) {
